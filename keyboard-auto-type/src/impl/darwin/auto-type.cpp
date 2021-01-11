61ed9b0624dd5fa0c69ba6b9630384cdf6ac83b0
antelle <dmitryvit@gmail.com>
2021-01-11 23:57:46 +0100

    character decoding

--- keyboard-auto-type/src/impl/darwin/auto-type.cpp
+++ keyboard-auto-type/src/impl/darwin/auto-type.cpp
@@ -10,6 +10,10 @@ namespace keyboard_auto_type {
 class AutoType::AutoTypeImpl {
   private:
     auto_release<CGEventSourceRef> event_source_ = CGEventSourceCreate(kCGEventSourceStateHIDSystemState);
+    
+    static constexpr int KEYBOARD_LAYOUT_LENGTH = 128;
+    CGKeyCode keyboard_layout_[KEYBOARD_LAYOUT_LENGTH];
+    bool keyboard_layout_read_ = false;
 
   public:
     AutoTypeResult validate_system_state() {
@@ -81,35 +85,79 @@ class AutoType::AutoTypeImpl {
 
         return AutoTypeResult::Ok;
     }
+    
+    void read_keyboard_layout() {
+        // https://stackoverflow.com/questions/1918841/how-to-convert-ascii-character-to-cgkeycode
+        // https://stackoverflow.com/questions/8263618/convert-virtual-key-code-to-unicode-string
+        
+        if (keyboard_layout_read_) {
+            return;
+        }
 
-    CGEventFlags modifier_to_flags(Modifier modifier) {
-        if (modifier == Modifier::None) {
+        auto_release keyboard = TISCopyCurrentKeyboardInputSource();
+        auto_release layout_data = (CFDataRef)TISGetInputSourceProperty(keyboard, kTISPropertyUnicodeKeyLayoutData);
+        auto layout = (const UCKeyboardLayout *)CFDataGetBytePtr(layout_data);
+        auto kbd_type = LMGetKbdType();
+
+        uint32_t keys_down = 0;
+        UniChar chars[4];
+        UniCharCount length;
+
+        for (int i = 0; i < KEYBOARD_LAYOUT_LENGTH; i++) {
+            keyboard_layout_[i] = 0;
+        }
+        for (int code = 0; code < 128; code++) {
+            auto trans = UCKeyTranslate(layout, code, kUCKeyActionDisplay, 0, kbd_type, kUCKeyTranslateNoDeadKeysBit, &keys_down, 4,
+                           &length, chars);
+
+            auto_release str = CFStringCreateWithCharacters(kCFAllocatorDefault, chars, 10);
+            auto ch = CFStringGetCharacterAtIndex(str, 0);
+
+            if (length && (ch >= '!' && ch <= '~') && !keyboard_layout_[ch]) {
+                keyboard_layout_[ch] = code;
+            }
+        }
+        
+        keyboard_layout_read_ = true;
+    }
+
+    CGKeyCode char_to_key_code(wchar_t character) {
+        read_keyboard_layout();
+        if (character < 0 || character >= KEYBOARD_LAYOUT_LENGTH) {
             return 0;
         }
-
-        CGEventFlags flags = 0;
-        if ((modifier & Modifier::Command) == Modifier::Command) {
-            flags |= kCGEventFlagMaskCommand;
-        }
-        if ((modifier & Modifier::Option) == Modifier::Option) {
-            flags |= kCGEventFlagMaskAlternate;
-        }
-        if ((modifier & Modifier::Control) == Modifier::Control) {
-            flags |= kCGEventFlagMaskControl;
-        }
-        if ((modifier & Modifier::Shift) == Modifier::Shift) {
-            flags |= kCGEventFlagMaskShift;
-        }
-        return flags;
+        auto index = std::tolower(character);
+        return keyboard_layout_[index];
     }
 };
+
+CGEventFlags modifier_to_flags(Modifier modifier) {
+    if (modifier == Modifier::None) {
+        return 0;
+    }
+
+    CGEventFlags flags = 0;
+    if ((modifier & Modifier::Command) == Modifier::Command) {
+        flags |= kCGEventFlagMaskCommand;
+    }
+    if ((modifier & Modifier::Option) == Modifier::Option) {
+        flags |= kCGEventFlagMaskAlternate;
+    }
+    if ((modifier & Modifier::Control) == Modifier::Control) {
+        flags |= kCGEventFlagMaskControl;
+    }
+    if ((modifier & Modifier::Shift) == Modifier::Shift) {
+        flags |= kCGEventFlagMaskShift;
+    }
+    return flags;
+}
 
 AutoType::AutoType() : impl_(std::make_unique<AutoType::AutoTypeImpl>()) {}
 
 AutoType::~AutoType() = default;
 
 AutoTypeResult AutoType::key_move(Direction direction, wchar_t character, KeyCode code, Modifier modifier) {
-    auto flags = impl_->modifier_to_flags(modifier);
+    auto flags = modifier_to_flags(modifier);
     return impl_->key_up_down(character, map_key_code(code), flags, direction == Direction::Down);
 }
 
@@ -122,6 +170,8 @@ AutoTypeResult AutoType::key_press(wchar_t character, KeyCode code, Modifier mod
         return AutoTypeResult::BadArg;
     }
 
+    auto native_key_code = code == KeyCode::Undefined ? impl_->char_to_key_code(character) : map_key_code(code);
+
     CGEventFlags flags = 0;
     AutoTypeResult result;
 
@@ -130,10 +180,10 @@ AutoTypeResult AutoType::key_press(wchar_t character, KeyCode code, Modifier mod
         if (result != AutoTypeResult::Ok) {
             return result;
         }
-        flags = impl_->modifier_to_flags(modifier);
+        flags = modifier_to_flags(modifier);
     }
 
-    result = impl_->key_up_and_down(character, map_key_code(code), flags);
+    result = impl_->key_up_and_down(character, native_key_code, flags);
     if (result != AutoTypeResult::Ok) {
         return result;
     }
@@ -158,11 +208,12 @@ AutoTypeResult AutoType::text(std::wstring text, Modifier modifier) {
         if (result != AutoTypeResult::Ok) {
             return result;
         }
-        flags = impl_->modifier_to_flags(modifier);
+        flags = modifier_to_flags(modifier);
     }
 
     for (auto character : text) {
-        result = impl_->key_up_and_down(character, 0, flags);
+        auto native_key_code = impl_->char_to_key_code(character);
+        result = impl_->key_up_and_down(character, native_key_code, flags);
         if (result != AutoTypeResult::Ok) {
             return result;
         }
