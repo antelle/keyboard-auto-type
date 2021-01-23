0d09626f14f706712b3769b343aa1625c60f8b60
antelle <dmitryvit@gmail.com>
2021-01-23 20:59:24 +0100

    reading keyboard layout on linux

--- keyboard-auto-type/src/impl/linux/auto-type.cpp
+++ keyboard-auto-type/src/impl/linux/auto-type.cpp
@@ -3,6 +3,7 @@
 #include <X11/Xutil.h>
 #include <X11/extensions/XTest.h>
 
+#include <algorithm>
 #include <iostream>
 
 #undef None
@@ -17,8 +18,15 @@ namespace keyboard_auto_type {
 
 class AutoType::AutoTypeImpl {
   private:
+    struct KeyCodeWithShiftLevel {
+        int key_code;
+        int shift_level;
+    };
+
     Display *display_ = nullptr;
     std::optional<bool> is_supported_;
+    std::optional<uint8_t> active_layout_;
+    std::unordered_map<KeySym, KeyCodeWithShiftLevel> keyboard_layout_ = {};
 
   public:
     ~AutoTypeImpl() {
@@ -52,20 +60,67 @@ class AutoType::AutoTypeImpl {
         if (!is_supported()) {
             return throw_or_return(AutoTypeResult::NotSupported, "Not supported");
         }
-        if (!display_) {
+        if (!display()) {
             return throw_or_return(AutoTypeResult::OsError, "Cannot open display");
         }
-        auto keycode = XKeysymToKeycode(display_, code);
-        if (!keycode) {
+
+        auto layout_entry = keyboard_layout_.find(code);
+        if (layout_entry == keyboard_layout_.end()) {
             return throw_or_return(AutoTypeResult::BadArg,
                                    std::string("Bad key code: ") + std::to_string(code));
         }
+        auto key_code_with_shift_level = layout_entry->second;
+        auto key_code = key_code_with_shift_level.key_code;
+
+        // TODO: shift level
+
         auto down = direction == Direction::Down;
-        auto res = XTestFakeKeyEvent(display_, keycode, down, CurrentTime);
+        auto res = XTestFakeKeyEvent(display(), key_code, down, CurrentTime);
         if (!res) {
             return throw_or_return(AutoTypeResult::OsError, "Failed to send an event");
         }
         return AutoTypeResult::Ok;
+    }
+
+    void read_keyboard_layout() {
+        if (!display()) {
+            return;
+        }
+
+        XkbStateRec kbd_state;
+        if (XkbGetState(display(), XkbUseCoreKbd, &kbd_state)) {
+            return;
+        }
+        auto active_layout = kbd_state.group;
+        if (active_layout == active_layout_) {
+            return;
+        }
+
+        keyboard_layout_.clear();
+
+        auto kbd_components = XkbCompatMapMask | XkbGeometryMask; // XkbAllComponentsMask
+        auto kbd = XkbGetKeyboard(display(), kbd_components, XkbUseCoreKbd);
+        if (!kbd) {
+            return;
+        }
+
+        for (auto key_code = kbd->min_key_code; key_code < kbd->max_key_code; key_code++) {
+            auto shift_levels_count = XkbKeyGroupWidth(kbd, key_code, kbd_state.group);
+            auto max_shift_level = std::min(static_cast<uint8_t>(1U), shift_levels_count);
+            for (auto shift_level = 0; shift_level <= max_shift_level; shift_level++) {
+                auto sym = XkbKeySymEntry(kbd, key_code, shift_level, kbd_state.group);
+                if (sym && !keyboard_layout_.count(sym)) {
+                    KeyCodeWithShiftLevel key_code_with_shift_level{};
+                    key_code_with_shift_level.key_code = key_code;
+                    key_code_with_shift_level.shift_level = shift_level;
+                    keyboard_layout_.emplace(sym, key_code_with_shift_level);
+                }
+            }
+        }
+
+        active_layout_ = active_layout;
+
+        XkbFreeKeyboard(kbd, kbd_components, True);
     }
 };
 
@@ -73,9 +128,15 @@ AutoType::AutoType() : impl_(std::make_unique<AutoType::AutoTypeImpl>()) {}
 
 AutoType::~AutoType() = default;
 
-AutoTypeResult AutoType::key_move(Direction direction, char32_t, std::optional<os_key_code_t> code,
-                                  Modifier) {
-    return impl_->key_move(direction, code.value_or(0));
+AutoTypeResult AutoType::key_move(Direction direction, char32_t character,
+                                  std::optional<os_key_code_t> code, Modifier) {
+    if (!code.has_value()) {
+        auto msg = std::string("Character ") + std::to_string(static_cast<uint32_t>(character)) +
+                   " not supported";
+        return throw_or_return(AutoTypeResult::BadArg, msg);
+    }
+    impl_->read_keyboard_layout(); // TODO: optimize for batch input
+    return impl_->key_move(direction, code.value());
 }
 
 Modifier AutoType::get_pressed_modifiers() {
@@ -94,6 +155,7 @@ std::optional<os_key_code_t> AutoType::os_key_code(KeyCode code) {
 }
 
 std::optional<KeyCodeWithModifiers> AutoType::os_key_code_for_char(char32_t character) {
+    impl_->read_keyboard_layout();
     auto keysym = char_to_keysym(character);
     if (!keysym) {
         return std::nullopt;
@@ -105,10 +167,16 @@ std::optional<KeyCodeWithModifiers> AutoType::os_key_code_for_char(char32_t char
 
 std::vector<std::optional<KeyCodeWithModifiers>>
 AutoType::os_key_codes_for_chars(std::u32string_view text) {
+    impl_->read_keyboard_layout();
     std::vector<std::optional<KeyCodeWithModifiers>> result(text.length());
     auto length = text.length();
     for (size_t i = 0; i < length; i++) {
-        result[i] = os_key_code_for_char(text[i]);
+        auto keysym = char_to_keysym(text[i]);
+        if (keysym) {
+            KeyCodeWithModifiers kc{};
+            kc.code = keysym;
+            result[i] = kc;
+        }
     }
     return result;
 }
