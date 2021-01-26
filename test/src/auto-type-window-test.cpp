95eb5eb9dd66cbdc19cb24510fe579556eaf18f9
antelle <dmitryvit@gmail.com>
2021-01-26 23:45:09 +0100

    added a test for no window

--- test/src/auto-type-window-test.cpp
+++ test/src/auto-type-window-test.cpp
@@ -12,12 +12,22 @@ namespace kbd = keyboard_auto_type;
 namespace keyboard_auto_type_test {
 
 class AutoTypeWindowTest : public testing::Test {
+  private:
+    bool test_app_closed_ = false;
+
   protected:
     static void SetUpTestSuite() {}
 
     virtual void SetUp() {}
 
-    virtual void TearDown() { save_text_and_close_test_app(); }
+    virtual void TearDown() { close_test_app(); }
+
+    void close_test_app() {
+        if (!test_app_closed_) {
+            save_text_and_close_test_app();
+            test_app_closed_ = true;
+        }
+    }
 };
 
 TEST_F(AutoTypeWindowTest, active_pid) {
@@ -105,6 +115,40 @@ TEST_F(AutoTypeWindowTest, show_window) {
             win = typer.active_window();
             ASSERT_EQ(win.pid, typer.active_pid());
             ASSERT_EQ(editor_window.pid, win.pid);
+
+            return;
+        }
+    }
+
+    FAIL() << "Text editor didn't start";
+}
+
+TEST_F(AutoTypeWindowTest, show_window_no_window) {
+    kbd::AutoType typer;
+
+    auto self_window = typer.active_window();
+    ASSERT_EQ(self_window.pid, typer.active_pid());
+    ASSERT_NE(0, self_window.pid);
+
+    open_test_app();
+    for (auto i = 0; i < 100; i++) {
+        wait_millis(100);
+        auto editor_window = typer.active_window();
+        ASSERT_EQ(editor_window.pid, typer.active_pid());
+        if (editor_window.pid != self_window.pid) {
+            wait_millis(1000);
+
+            auto win = typer.active_window();
+            ASSERT_EQ(win.pid, typer.active_pid());
+            ASSERT_EQ(editor_window.pid, win.pid);
+            close_test_app();
+
+            auto shown = typer.show_window(self_window);
+            ASSERT_TRUE(shown);
+            wait_millis(1000);
+
+            auto editor_shown = typer.show_window(editor_window);
+            ASSERT_FALSE(editor_shown);
 
             return;
         }
